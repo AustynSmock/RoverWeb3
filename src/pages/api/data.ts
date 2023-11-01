3bd53e8a74902dadb0b1fbc53df98c5abd34a55a
AngelNBazan <angelnaimbazan@gmail.com>
2023-10-31 21:19:02 -0700

    new

---
+++ src/pages/api/data.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// Variable to store the text data (just for the sake of example)
+let storedText = '';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    // Fetch data from the Express server
+    const response = await fetch('http://localhost:3000/data', {
+      method: 'GET'
+    });
+
+    const data = await response.json();
+    
+    // Return the data to the frontend
+    res.status(200).json(data);
+  } else if (req.method === 'POST') {
+    // If text data is sent via POST, store it
+    const { text } = req.body;
+    
+    if (text) {
+      storedText = text;
+      
+      // Send the text to the Express server
+      const response = await fetch('http://localhost:3000/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: storedText })
+      });
+
+      const expressResponse = await response.json();
+
+      res.status(200).json(expressResponse);
+    } else {
+      res.status(400).json({ error: 'Text data is required' });
+    }
+  } else {
+    res.status(405).json({ error: 'HTTP Method not allowed' });
+  }
+}
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const data = await fetch('http://localhost:3000/',{
+  method:'GET',
+  })
+  res.status(200).json(await data.json())
 }
